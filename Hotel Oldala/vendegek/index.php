0a1cb20fcfd014350620a30be7906fcccdb0186c
TheMenta <91569408+TheMenta@users.noreply.github.com>
2025-12-02 13:05:11 +0100

    Add navigation and update room display structure

--- Hotel Oldala/vendegek/index.php
+++ Hotel Oldala/vendegek/index.php
@@ -8,17 +8,30 @@ require '../../cfg.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Főoldal</title>
 </head>
 <body>
     <h1>Üdvözlünk a React Hotel oldalán!</h1>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Főoldal</li>
+            <li><a href="feoglalas.php">Foglalás</li>
+             <li><a href="reglog.php">Bejelentkezes</li>
+        </ul>
+    </nav>
 
     <?php
     $lekerdezes = "SELECT * FROM rooms";
     $talalt_sorok = $conn->query($lekerdezes);
+    echo "<div class='container'>";
     while($sor  = $talalt_sorok->fetch_assoc()){
-        echo "<p>".$sor['room_number']."</p>";
+        
+        echo "<div>"."<p>".$sor['room_number']."</p>"."</div>";
+       
     } 
+     echo "</div>";
     ?>
 </body>
 </html>
