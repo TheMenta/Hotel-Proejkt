887e8fb31ba6fe17c05dc9d31a8094288ca38144
TheMenta <91569408+TheMenta@users.noreply.github.com>
2025-12-16 13:11:34 +0100

    Enhance reservation form with date and quantity fields
    Updated reservation form to include date selection and quantity input.

--- Hotel Oldala/vendegek/reserv.php
+++ Hotel Oldala/vendegek/reserv.php
@@ -1,22 +1,41 @@
 <?php
 require '../../cfg.php';
-//get-tel majd le kerem az adott szoba dolgait.
+
 if(!isset($_COOKIE['id'])){
-    echo "<script>alert('Kerlek Jelentkezz be')</script>";
-    
+    echo "<script>alert('Kerlek Jelentkezz be tovabbi kedvezmenyekert!')</script>";
+     echo  "<form method='post'>";
+     echo   "<input type='email' name='email' placeholder='asd@asd.hu'>";
+     echo   "<input type='text' name='name' placeholder='Vezetek Kresztnev'>";
+     echo   "<input type='password'>";
+     echo   "</form>";
 }
 else{
-    $lekerdezes = "SELECT * FROM users WHERE id=$_COOKIE[id]";
-    $talalt_felhasznalo = $conn->query($lekerdezes);
-    $felhasznalo = $talalt_felhasznalo->fetch_assoc();
+    
+}
+echo "<h1>eleherto szobak</h1>";
+$lekerdezes = "SELECT * FROM rooms where status = 0";
+$talalt_sorok = $conn->query($lekerdezes);
+while($sor = $talalt_sorok->fetch_assoc()){
+     echo "<div>"."<p>".$sor['room_number']."</p>"."</div>";
+
 }
 
-// Nézett profil lekérdezése
-$lekerdezes = "SELECT * FROM users WHERE id=$_GET[roomid]";
-$talalt_szoba = $conn->query($lekerdezes);
-$profil = $talalt_szoba->fetch_assoc();
 
 
+
+
+
+
+if(isset($_POST['btn'])){
+$_POST['start_d'] = $strat_d;
+$_POST['end_d'] = $end_d;
+$_POST['quantity'] = $quantity;
+$lekerdezes = "SELECT * FROM bookings WHERE ";
+$talalt_sorok = $conn->query($lekerdezes);
+while($sor = $talalt_sorok->fetch_assoc()){
+
+}
+}
 ?>
 <!DOCTYPE html>
 <html lang="hu">
@@ -26,11 +45,12 @@ $profil = $talalt_szoba->fetch_assoc();
     <title>Foglalás</title>
 </head>
 <body>
-<!-- ez majd csak akkor jelenjen meg ha valaki nincs bejelentkezve -->
     <form method="post">
-    <input type="email" name="email" placeholder="asd@asd.hu">
-    <input type="text" name="name" placeholder="Vezetek Kresztnev">
-    <input type="password">
+        <input type="date" name="start_d">
+        <input type="date" name="end_d">
+        <input type="number" id="quantity" name="quantity" min="1" max="6">
+        <input type="submit" name="btn" value="kereses">
     </form>
+  
 </body>
 </html>
